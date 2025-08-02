df149d028992dca1d0fd15376f9849259c1c6802
hasnayamba <yambahassana@gmail.com>
2025-08-02 12:06:37 +0100

    YMLUPDATE

--- gestion_absencesApp/deployment.py
+++ gestion_absencesApp/deployment.py
@@ -20,27 +20,27 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-parameters = {}
-for pair in connection_string.split(' '):
-    key, value = pair.split('=', 1)
-    parameters[key] = value
 
+# Configuration de la base de données pour Azure PostgreSQL
+# Parsing de la chaîne de connexion
+url = urlparse(connection_string)
 
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': parameters['dbname'],
-        'USER': parameters['user'],
-        'PASSWORD': parameters['password'],
-        'HOST': parameters['host'],
-        'PORT': parameters['port'],
-        'OPTIONS': {
-            'sslmode': parameters.get('sslmode', 'require'),
-        },
+        'NAME': url.path[1:],  # retire le slash initial
+        'USER': url.username,
+        'PASSWORD': url.password,
+        'HOST': url.hostname,
+        'PORT': url.port,
     }
 }
+
+print("HOST utilisé pour PostgreSQL:", url.hostname)
